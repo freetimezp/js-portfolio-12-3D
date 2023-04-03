3022e2bffa3cd831f0694fdf6997a5754da5ec65
freetimezp <kraftukrnet@ukr.net>
2023-04-03 18:25:50 +0300

    09. js-portfolio-12-3d. ColorPicker component.

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>
-      ColorPicker
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        presetColors={[
+          '#ccc', '#efbd4e', '#80c670', 
+          '#726de8', '#353934', '#2cccE4', 
+          '#ff8a65', '#7098da', '#c19277', 
+          '#ff96ad', '#512314', '#5f123d'
+        ]}
+        onChange={(color) => state.color = color.hex}
+      />
     </div>
   );
 }
